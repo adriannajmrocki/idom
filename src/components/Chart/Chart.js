18c824ff3eb8b0a8f486372be53ef40c15ec70aa
Adrian Najmrocki <adrian.najmrocki@gmail.com>
2020-12-02 14:18:42 +0100

    Buttons for charts data filtering

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -6,6 +6,7 @@ import { Line } from 'react-chartjs-2';
 import { getChartData } from '../../actions/sensors';
 
 import '../../styles/utilStyles.css';
+import './style.css';
 
 const Chart = (props) => {
 
@@ -14,12 +15,19 @@ const Chart = (props) => {
     props.getChartData(id);
   }, []);
 
+  const allData = props.chartData.map(data => {
+    const date = new Date(data.delivery_time).toLocaleDateString();
+    const time = new Date(data.delivery_time).toLocaleTimeString();
+    return `${time} | ${date}`;
+  })
+
   const chartData = {
-    labels: props.chartData.map(data => {
-      const date = new Date(data.delivery_time).toLocaleDateString();
-      const time = new Date(data.delivery_time).toLocaleTimeString();
-      return `${time} | ${date}`;
-    }),
+    // labels: props.chartData.map(data => {
+    //   const date = new Date(data.delivery_time).toLocaleDateString();
+    //   const time = new Date(data.delivery_time).toLocaleTimeString();
+    //   return `${time} | ${date}`;
+    // }),
+    labels: allData,
     datasets: [
       {
         data: props.chartData.map(data => data.sensor_data),
@@ -32,9 +40,14 @@ const Chart = (props) => {
 
   return (  
     <div className="container">
-      <div className="head" style={{'marginBottom': '50px'}}>
+      <div className="head" style={{'marginBottom': '30px'}}>
         <h2>Wykres</h2>
         <div className="line" style={{"width": "900px"}}></div>
+      </div>
+      <div className="filter-buttons">
+        <button className="month filter-btn">30 dni</button>
+        <button className="two-weeks filter-btn">14 dni</button>
+        <button className="day filter-btn">1 dzień</button>
       </div>
       <div className="chart">
         <Line
@@ -57,74 +70,3 @@ const mapStateToProps = state => ({
 })
  
 export default connect(mapStateToProps, { getChartData })(Chart);
-
-
-// import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
-// import { Line } from 'react-chartjs-2';
-
-// import { getChartData } from '../../actions/sensors';
-
-// import '../../styles/utilStyles.css';
-
-// class Chart extends Component {
-//   state = {
-//     chartData: {
-//       labels: this.props.chartData.map(data => {
-//         const date = new Date(data.delivery_time).toLocaleDateString();
-//         const time = new Date(data.delivery_time).toLocaleTimeString();
-//         return `${time} | ${date}`;
-//       }),
-//       datasets: [
-//         {
-//           data: this.props.chartData.map(data => data.sensor_data),
-//           fill: false,
-//           backgroundColor: '#987316',
-//           borderColor: 'rgba(152, 115, 22, 0.2)',
-//         },
-//       ],
-//     }
-//   }
-
-//   static propTypes = {
-//     getChartData: PropTypes.func.isRequired
-//   }
-
-//   componentDidMount() {
-//     const id = this.props.match.params.id;
-//     this.props.getChartData(id);
-//   }
-
-//   render() { 
-
-//     const { chartData } = this.state;
-
-//     return (  
-//       <div className="container">
-//         <div className="head" style={{'marginBottom': '50px'}}>
-//           <h2>Wykres</h2>
-//           <div className="line" style={{"width": "900px"}}></div>
-//         </div>
-//         <div className="chart">
-//           <Line
-//             data={chartData}
-//             options={{
-//               maintainAspectRatio: false,
-//               legend: {
-//                 display: false,
-//               }
-//             }}
-//             height={400}
-//           />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// const mapStateToProps = state => ({
-//   chartData: state.sensors.chartData,
-// })
- 
-// export default connect(mapStateToProps, { getChartData })(Chart);
