2a5a9ef290be09c5f3aa2decb7d95c5980556fdb
Adrian Najmrocki <adrian.najmrocki@gmail.com>
2020-12-22 00:51:18 +0100

    Home page update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -256,21 +256,6 @@ class Home extends Component {
           </div>
         </div>
       </div>
-  
-  
-      // <Fragment>
-      //   <div className="jumbotron" style={{ backgroundColor: "#fff" }}>
-      //     <h1 className="display-3">IDOM</h1>
-      //     <p className="lead">{t('TWÓJ INTELIGENTNY DOM W JEDNYM MIEJSCU')}</p>
-      //     <hr className="my-4" />
-      //     <p>Dzięki aplikacji IDOM możesz kontrolować swój inteligentny dom nie ruszając się z miejsca. <br /> Dołącz do nas i zobacz jakie to proste!</p>
-      //     <p className="lead">
-      //       <Link to="/register"><button className="btn btn-primary btn-lg">Dołącz do nas!</button></Link>
-      //     </p>
-      //     <button onClick={() => i18n.changeLanguage('pl')}>PL</button>
-      //     <button onClick={() => i18n.changeLanguage('en')}>EN</button>
-      //   </div>
-      // </Fragment>
     );
 
   }
