173c26eccb2ddb438444430ce61519e09ddc34ab
Adrian Najmrocki <adrian.najmrocki@gmail.com>
2020-12-30 21:27:27 +0100

    CSV translation

--- src/components/Csv/Csv.js
+++ src/components/Csv/Csv.js
@@ -1,22 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import { useTranslation } from "react-i18next";
 
 import { getSensors, postCsv } from '../../actions/sensors';
 import { createMessage } from '../../actions/messages';
 import Alerts from '../Alerts/Alerts';
 
-const categories = [
-  { id: 1, isChecked: false, value: "temperature", label: "Temperatura" },
-  { id: 2, isChecked: false, value: "water_temp", label: "Temperatura wody" },
-  { id: 3, isChecked: false, value: "humidity", label: "Wilgotność" },
-  { id: 4, isChecked: false, value: "air_humidity", label: "Wilgotność powietrza" },
-  { id: 5, isChecked: false, value: "atmo_pressure", label: "Ciśnienie atmosferyczne" },
-  { id: 6, isChecked: false, value: "breathalyser", label: "Alkomat" },
-  { id: 7, isChecked: false, value: "smoke", label: "Dym" },
-  { id: 8, isChecked: false, value: "rain_sensor", label: "Deszcz" },
-]
+// const categories = [
+//   { id: 1, isChecked: false, value: "temperature", label: `${t('sensors.temp')}` },
+//   { id: 2, isChecked: false, value: "water_temp", label: "Temperatura wody" },
+//   { id: 3, isChecked: false, value: "humidity", label: "Wilgotność" },
+//   { id: 4, isChecked: false, value: "air_humidity", label: "Wilgotność powietrza" },
+//   { id: 5, isChecked: false, value: "atmo_pressure", label: "Ciśnienie atmosferyczne" },
+//   { id: 6, isChecked: false, value: "breathalyser", label: "Alkomat" },
+//   { id: 7, isChecked: false, value: "smoke", label: "Dym" },
+//   { id: 8, isChecked: false, value: "rain_sensor", label: "Deszcz" },
+// ]
 
 function Csv(props) {
+
+  const { t } = useTranslation('common');
+
+  const categories = [
+    { id: 1, isChecked: false, value: "temperature", label: `${t('sensors.temp')}` },
+    { id: 2, isChecked: false, value: "water_temp", label: `${t('sensors.water-temp')}` },
+    { id: 3, isChecked: false, value: "humidity", label: `${t('sensors.hum')}` },
+    { id: 4, isChecked: false, value: "air_humidity", label: `${t('sensors.air-hum')}` },
+    { id: 5, isChecked: false, value: "atmo_pressure", label: `${t('sensors.pressure')}` },
+    { id: 6, isChecked: false, value: "breathalyser", label: `${t('sensors.alc')}` },
+    { id: 7, isChecked: false, value: "smoke", label: `${t('sensors.smoke')}` },
+    { id: 8, isChecked: false, value: "rain_sensor", label: `${t('sensors.rain')}` },
+  ]
 
   const [filter, setFilter] = useState('');
   const [sensorsState, setSensorsState] = useState([]);
@@ -80,7 +94,7 @@ function Csv(props) {
     <div className="container">
       <div className="head" style={{"marginBottom": "40px"}}>
         <i className="far fa-file-alt fa-2x"></i>
-        <h2>Plik CSV</h2>
+        <h2>{t('csv-generate.csv-file')}</h2>
         <div className="line"></div>
       </div>
   
@@ -88,11 +102,11 @@ function Csv(props) {
         <div className="card card-body mt-5">
           <form onSubmit={handleSubmit}>
             <div className="form-group">
-              <label>Wybierz filtr</label>
+              <label>{t('csv-generate.csv-filter')}</label>
               <select className="form-control" onChange={handleFilterSelect} value={filter}>
                 <option value="" disabled defaultValue></option>
-                <option value="sensors">Czujniki</option>
-                <option value="categories">Kategorie</option>
+                <option value="sensors">{t('header.sensors')}</option>
+                <option value="categories">{t('csv-generate.categories')}</option>
               </select>
             </div>
   
@@ -157,7 +171,7 @@ function Csv(props) {
             : false }
   
             <div className="form-group">
-              <label>Dni</label>
+              <label>{t('sensors.days')}</label>
               <input 
                 type="text"
                 className="form-control"
@@ -168,7 +182,7 @@ function Csv(props) {
             </div>
   
             <div className="form-group">
-              <button className="btn btn-primary">Dodaj</button>
+              <button className="btn btn-primary">{t('sensors.add')}</button>
             </div>
           </form>
         </div>
