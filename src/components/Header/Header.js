90b08a0c0fa6e49cce5cec0f432f8ea2bae7749c
Adrian Najmrocki <adrian.najmrocki@gmail.com>
2020-12-30 13:42:33 +0100

    Header translation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { withTranslation } from 'react-i18next';
 
 import { logout } from '../../actions/auth';
 
@@ -16,18 +17,19 @@ class Header extends Component {
 
     const { isAuthenticated } = this.props.auth;
     const { isMenuVisible } = this.state;
+    const { t } = this.props;
 
     const authLinks = (
       <Fragment>
         <label htmlFor="check" className="checkbtn"><i className="fas fa-bars" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}></i></label>
         <label className="brand-name"><Link to="board">IDOM</Link></label>
         <ul className={`${isMenuVisible ? 'show-menu' : ''}`}>
-          <li><NavLink to="/dashboard" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}>Czujniki</NavLink></li>
-          <li><NavLink to="/controllers" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}>Sterowniki</NavLink></li>
-          <li><NavLink to="/actions" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}>Akcje</NavLink></li>
-          <li><NavLink to="/cameras" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}>Kamery</NavLink></li>
-          <li><NavLink to="/admin" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}>Konto</NavLink></li>
-          <button onClick={this.props.logout}>Wyloguj</button>
+          <li><NavLink to="/dashboard" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}>{t('header.sensors')}</NavLink></li>
+          <li><NavLink to="/controllers" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}>{t('header.controllers')}</NavLink></li>
+          <li><NavLink to="/actions" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}>{t('header.actions')}</NavLink></li>
+          <li><NavLink to="/cameras" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}>{t('header.cameras')}</NavLink></li>
+          <li><NavLink to="/admin" onClick={() => this.setState({ isMenuVisible: !isMenuVisible })}>{t('header.account')}</NavLink></li>
+          <button onClick={this.props.logout}>{t('header.logout')}</button>
         </ul>
       </Fragment>  
     );
@@ -44,4 +46,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 })
 
-export default connect(mapStateToProps, { logout })(Header);
+export default withTranslation('common')(connect(mapStateToProps, { logout })(Header));
